f00ef61860e1ba53f6292cd3714aff03b5f4a7fa
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-06-03 09:51:12 +0700

    reset password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,19 @@
 const { use } = require("../routes/auth.route");
 const User = require("./../models/user");
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const config_mail = {
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: "hoatq4@fpt.edu.vn",
+        pass: "vixndyrjlvetuntr"
+    }
+}
+const transport = nodemailer.createTransport(config_mail);
+
+
 exports.register = (req,res)=>{
     const auth = req.session.auth;
     if(auth) return res.send(`Bạn đã login bằng email ${auth.email}`);
@@ -45,5 +58,61 @@ exports.loginUser = async (req,res)=>{
         res.send(error);
     }
 }
+
+exports.logout = (req,res)=>{
+    req.session.auth = null;
+    req.redirect("/auth/login");
+}
+
+exports.form_forgot = (req,res)=>{
+    res.render("auth/forgot");
+}
+exports.forgot = async (req,res)=>{
+    const email = req.body.email;
+    try {
+        const user = await User.findOne({email:email});
+        if(!user) return res.redirect("/auth/forgot-password");
+        // neu co email: 
+        const randomStr = btoa(user.email);
+        const linkReset = "http://localhost:3000/auth/reset-password?code="+randomStr;
+        req.session.resetpassword = {
+            user: user,
+            code: randomStr
+        };
+        // send email
+        transport.sendMail({
+            from: "Demo NodeJS T2204M",
+            to: user.email,
+            cc:"",
+            subject: "Lấy lại mật khẩu tài khoản",
+            html: `<p>Click <a href="${linkReset}">here</a> to reset password.</p>`
+        });
+        res.send("VUi lòng kiểm tra email và làm theo hướng dẫn");
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+exports.form_reset = (req,res)=>{
+    //check code
+    const code = req.query.code;
+    const resetSession = req.session.resetpassword;
+    if(code != resetSession.code) return res.status(404).send("Error");
+    res.render("auth/reset");
+}
+exports.reset = async (req,res)=>{
+    const new_pass = req.body.password;
+    const resetSession = req.session.resetpassword;
+    const user = resetSession.user;
+    const salt = await bcrypt.genSalt(10);
+    const hashPwd = await bcrypt.hash(new_pass,salt);
+    
+    await User.findByIdAndUpdate(user._id,{
+        password:hashPwd
+    });
+    req.session.resetpassword = null;
+    res.sedn("done");
+}
+
 
 // change password
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,5 +4,12 @@ const router = express.Router();
 const controller = require("./../controllers/auth.controller");
 router.get("/register",controller.register);
 router.post("/register",controller.create_user);
+router.post("/logout",controller.logout);
+
+router.get("/forgot-password",controller.form_forgot);
+router.post("/forgot-password",controller.forgot);
+
+router.get("/reset-password",controller.form_reset);
+router.post("/reset-password",controller.reset);
 
 module.exports = router;
