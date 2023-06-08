87973001f57c9a4ac6fd860463ac92081bdf50ee
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-06-08 09:40:59 +0700

    upload file - base64

--- src/models/product.js
+++ src/models/product.js
@@ -3,7 +3,11 @@ const product = new mongoose.Schema({
     name:String,
     price: Number,
     qty: Number,
-    brand:String
+    brand:String,
+    thumbnail:{
+        data: String,
+        contentType:String
+    }
 });
 
 module.exports = mongoose.model("Product",product);
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -6,17 +6,30 @@ const controller = require("./../controllers/product.controller");
 const middleware = require("./../middlewares/auth.middleware");
 
 // nếu muốn toàn bộ route đều bị lọc qua middleware 
-router.use(middleware.logged);
-router.use((req,res,next)=>{
-    const auth = req.session.auth;
-    if(auth.permissions.include(MODULE)){
-        return next();
+// router.use(middleware.logged);
+// router.use((req,res,next)=>{
+//     const auth = req.session.auth;
+//     if(auth.permissions.include(MODULE)){
+//         return next();
+//     }
+//     res.status(404).send("Error");
+// })
+
+// upload file
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"public/uploads/products");
+    },
+    filename: function(req,file,cb){
+       // console.log(file);
+        cb(null,Date.now()+"-"+file.originalname);
     }
-    res.status(404).send("Error");
-})
+});
+const upload = multer({storage:storage});
 
 router.get("/",controller.get);
 router.get("/create",controller.create);
-router.post("/create",controller.save);
+router.post("/create",upload.single("thumbnail"),controller.save);
 
 module.exports = router;
